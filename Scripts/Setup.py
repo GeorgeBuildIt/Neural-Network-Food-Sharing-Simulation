d7bd0770e7b3fd2bfc5fb09651ca79e564049d2a
George <152743030+GeorgeBuildIt@users.noreply.github.com>
2026-02-03 20:16:41 +0000

    Update Setup.py

--- Scripts/Setup.py
+++ Scripts/Setup.py
@@ -4,12 +4,13 @@ import pybind11
 ext_modules = [
     Extension(
         'simulation',
-        ['Simulation.cpp', 'Main.cpp'], # Compile both files
+        ['Simulation.cpp', 'Main.cpp'], #Compile files
         include_dirs=[pybind11.get_include()],
         language='c++'
     ),
 ]
 
 setup(name='simulation', ext_modules=ext_modules)
+
 
 #python setup.py build_ext --inplace
